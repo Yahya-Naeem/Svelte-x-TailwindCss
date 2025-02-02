692f07032c916e141c3e7d15f0a5e961832edf56
Yahya-Naeem <yahyanaeemofficial@gmail.com>
2025-02-03 02:54:14 +0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- //Home page -->
+<script>
+    import '../app.css';
+</script>
+
+<div>
+    <h1 class="text-3xl font-bold text-blue-500">
+        Svelte app x Tailwind styling
+    </h1>
+    <p>
+        Text
+    </p>
+
+</div>
